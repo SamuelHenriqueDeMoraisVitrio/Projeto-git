9a0255e03ee142ccf315e4e4a79476000bacf4de
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2021-06-05 09:46:28 -0300

    Segunda pergunta 2.0.

--- Código.js
+++ Código.js
@@ -1,10 +1,16 @@
-let texto = document.getElementById('text1')
+let texto1 = document.getElementById('text1')
+let texto2 = document.getElementById('text2')
+let texto21 = document.getElementById('text21')
+let texto3 = document.getElementById('text3')
 
 function cha1() {
     let numMax = document.getElementById('num')
     numMax = Number(numMax.value)
-    texto.innerHTML = (`<p>Escolha um numero da sorte, menor que ${numMax}, e maior que 0: <input type='number' id='sorte'/><input type='button' value='Calc' onclick='cha2()'/></p>`)
-    function cha2(params) {
-        
-    }
+    this.numMax1 = numMax
+    texto1.style.opacity = '0'
+    texto2.style.opacity = '1'
+}
+
+function cha2() {
+    
 }
