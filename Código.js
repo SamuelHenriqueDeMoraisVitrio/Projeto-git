b6881f4249c19330e80c782b86a24ba6e5f43c4c
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2021-06-04 23:26:16 -0300

    Pronta segunda pergunta e começando a interatividade da segunda pergunta.

--- Código.js
+++ Código.js
@@ -3,5 +3,8 @@ let texto = document.getElementById('text1')
 function cha1() {
     let numMax = document.getElementById('num')
     numMax = Number(numMax.value)
-    texto.innerHTML += (numMax)
+    texto.innerHTML = (`<p>Escolha um numero da sorte, menor que ${numMax}, e maior que 0: <input type='number' id='sorte'/><input type='button' value='Calc' onclick='cha2()'/></p>`)
+    function cha2(params) {
+        
+    }
 }
