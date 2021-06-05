71881a45c609efe2988d93309c63f2ebdbae9485
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2021-06-05 10:07:07 -0300

    Segunda pergunta 3.0. Começando terceira pergunta.

--- Código.js
+++ Código.js
@@ -3,14 +3,17 @@ let texto2 = document.getElementById('text2')
 let texto21 = document.getElementById('text21')
 let texto3 = document.getElementById('text3')
 
-function cha1() {
+const cha1 = () => {
     let numMax = document.getElementById('num')
     numMax = Number(numMax.value)
     this.numMax1 = numMax
     texto1.style.opacity = '0'
     texto2.style.opacity = '1'
+    texto21.innerHTML = numMax
 }
 
-function cha2() {
+let valoresDaSorte = []
+
+const cha2 = () => {
     
 }
