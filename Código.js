52ecf7c75503608d83942d8ed5fa514254d8c264
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2021-06-04 23:06:43 -0300

    Primeiro teste funcional com js.

---
+++ Código.js
@@ -0,0 +1,7 @@
+let texto = document.getElementById('text1')
+
+function cha1() {
+    let numMax = document.getElementById('num')
+    numMax = Number(numMax.value)
+    texto.innerHTML += (numMax)
+}
